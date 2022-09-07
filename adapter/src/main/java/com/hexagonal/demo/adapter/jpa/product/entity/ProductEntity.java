235693f4a02733f7762107087ac75234d3beae78
Antilamer1709 <gValera2101>
2022-09-07 20:11:27 +0300

    init scripts

--- adapter/src/main/java/com/hexagonal/demo/adapter/jpa/product/entity/ProductEntity.java
+++ adapter/src/main/java/com/hexagonal/demo/adapter/jpa/product/entity/ProductEntity.java
@@ -3,16 +3,14 @@ package com.hexagonal.demo.adapter.jpa.product.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
-@Entity
 @Getter
 @Setter
+@Entity
+@Table(name = "product")
 public class ProductEntity {
 
     @Id
---
+++ application/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO product (id, name) VALUES (1, 'New');

6afc2272dd8f5eb404a434dbb9fedbbe1ad634c6
Antilamer1709 <gValera2101>
2022-09-10 16:24:13 +0300

    swagger

--- adapter/src/main/java/com/hexagonal/demo/gateway/rest/order/OrderController.java
+++ adapter/src/main/java/com/hexagonal/demo/gateway/rest/order/OrderController.java
@@ -3,6 +3,8 @@ package com.hexagonal.demo.gateway.rest.order;
 import com.hexagonal.demo.domain.port.api.order.OrderServicePort;
 import com.hexagonal.demo.gateway.rest.order.dto.CreateOrderDto;
 import com.hexagonal.demo.gateway.rest.order.dto.OrderDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import java.util.List;
 
 import static com.hexagonal.demo.gateway.rest.order.mapper.OrderRestMapper.ORDER_REST_MAPPER;
 
+@Tag(name = "order", description = "Order operations")
 @RestController
 @RequestMapping("orders")
 @RequiredArgsConstructor
@@ -17,11 +20,13 @@ public class OrderController {
 
     private final OrderServicePort orderServicePort;
 
+    @Operation(summary = "Get all orders")
     @GetMapping
     public List<OrderDto> getAllOrders() {
         return ORDER_REST_MAPPER.toDtoList(orderServicePort.getAllOrders());
     }
 
+    @Operation(summary = "Create a new order")
     @PostMapping
     public OrderDto createOrder(@RequestBody CreateOrderDto createOrderDto) {
         return ORDER_REST_MAPPER.toDto(orderServicePort.createOrder(ORDER_REST_MAPPER.toDomainModel(createOrderDto)));
--- adapter/src/main/java/com/hexagonal/demo/gateway/rest/product/ProductController.java
+++ adapter/src/main/java/com/hexagonal/demo/gateway/rest/product/ProductController.java
@@ -3,6 +3,9 @@ package com.hexagonal.demo.gateway.rest.product;
 import com.hexagonal.demo.gateway.rest.product.dto.ProductDetailsDto;
 import com.hexagonal.demo.gateway.rest.product.dto.ProductDto;
 import com.hexagonal.demo.domain.port.api.product.ProductServicePort;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +16,7 @@ import java.util.List;
 
 import static com.hexagonal.demo.gateway.rest.product.mapper.ProductRestMapper.PRODUCT_REST_MAPPER;
 
+@Tag(name = "product", description = "Product operations")
 @RestController
 @RequestMapping("products")
 @RequiredArgsConstructor
@@ -20,13 +24,15 @@ public class ProductController {
 
     private final ProductServicePort productServicePort;
 
+    @Operation(summary = "Get all available products")
     @GetMapping
     public List<ProductDto> getAllProducts() {
         return PRODUCT_REST_MAPPER.toDtoList(productServicePort.getAllProducts());
     }
 
+    @Operation(summary = "Find a product by id")
     @GetMapping("{id}")
-    public ProductDetailsDto getProductById(@PathVariable Integer id) {
+    public ProductDetailsDto getProductById(@Parameter(description = "Id of the product") @PathVariable Integer id) {
         return PRODUCT_REST_MAPPER.toProductDetailsDto(productServicePort.getProductById(id));
     }
 }
